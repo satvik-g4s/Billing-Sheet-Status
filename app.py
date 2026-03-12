16f8ff280b823f56bd990b4b2a6b2e97cfa5e8a9
satvik-g4s <satvik.pruthi@in.g4s.com>
2026-03-12 10:52:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if run:
                 
 
                 bfl.columns = bfl.columns.str.strip().str.lower()
-                st.write(list(bfl.columns))
+                st.write("if showing error, kindly upload mapping in csv format(save as csv in excel and upload)")
                 
                 bfl = bfl[["cust_no","so_locn","branch_finance_lead"]]
 
